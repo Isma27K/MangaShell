785fd2cb70ff5a0a0772ad0900651d03a57c8803
ismasalalu <binhelati@gmail.com>
2024-09-18 20:31:50 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+
+// ============================= Routes ================================
+
+import Home from './routes/home/Home.Routes';
+
+// ============================= App ===================================
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. saya suka ayam
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/routes/home/Home.Routes.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="App">
+            hello, this is isma. i will work hard to make some progress ($^$) < br/> chiokkkkk
+        </div>
+    )
+}
+
+export default Home;
